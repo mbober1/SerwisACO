fbb4bdba6a183afcb7041762b6be0b1037333f3b
Rafal <rafal.rzewucki@gmail.com>
2020-05-08 12:15:51 +0200

    Stage 2 v1

--- viewModules/pages/add_queue.php
+++ viewModules/pages/add_queue.php
@@ -1,19 +1,20 @@
-<?php 
+<?php
 
-if(!isLogged() || getUserPermissions()) header('Location: index.php?site=queue');
+if (!isLogged() || getUserPermissions())
+	header('Location: index.php?site=queue');
 else {
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        if (isset($_POST['carSelect']) && isset($_POST['failure'])) {
-            try {
-                print_r($_POST['carSelect']);
-                $query = $service->addToQueue($_POST['carSelect'], $_POST['failure']);
-                header('Location: index.php?site=queue');
-            } catch (Exception $e) {
-                $error = 'Wystąpił błąd';
-                print_r($e);
-            }
-        }
-    }
+	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+		if (isset($_POST['carSelect']) && isset($_POST['failure'])) {
+			try {
+				print_r($_POST['carSelect']);
+				$query = $service->addToQueue($_POST['carSelect'], $_POST['failure']);
+				header('Location: index.php?site=queue');
+			} catch (Exception $e) {
+				$error = 'Wystąpił błąd';
+				print_r($e);
+			}
+		}
+	}
 }
 
 echo '
@@ -22,15 +23,16 @@ echo '
 
 $rows = $service->getCars(getUserID());
 if ($rows) {
-    echo '
+	echo '
     <form id="addqueue" class="card container" method="post" style="padding: 15px;">
     <div class="field">
         <label class="label">Wybierz auto</label>
             <div class="control">';
 
-    echo '<select required name="carSelect" class="dropdown-content">';
-    while ($row = array_shift($rows)) echo "<option value=", $row["id"], ">", $row["brand"], " ", $row["model"], " | ", $row["vin"]," </option>";
-    echo '</select>
+	echo '<select required name="carSelect" class="dropdown-content">';
+	while ($row = array_shift($rows))
+		echo "<option value=", $row["id"], ">", $row["brand"], " ", $row["model"], " | ", $row["vin"], " </option>";
+	echo '</select>
         </div>
     </div>
     <div class="field">
@@ -48,21 +50,18 @@ if ($rows) {
     <a href="index.php?site=queue"><button class="button is-link is-light">Anuluj</button></a>
 </div>
 </div>
-</form>'; 
-
-
+</form>';
 } else {
-    echo '<div id="addqueue" class="card container" method="post" style="padding: 15px;">';
-    echo "Nie masz dodanych żadnych pojazdów";
-    echo '<div class="control"><a href="index.php?site=addcar"><button class="button is-link">Dodaj auto</button></a></div></div>';
+	echo '<div id="addqueue" class="card container" method="post" style="padding: 15px;">';
+	echo "Nie masz dodanych żadnych pojazdów";
+	echo '<div class="control"><a href="index.php?site=addcar"><button class="button is-link">Dodaj auto</button></a></div></div>';
 }
 
 echo '</div>
 </div>';
-
 ?>
 
-                    
+
 
 
 
